56380443d138dba9af7c6d1cee36f359dda4d321
faddies <faddies.sofizar@gmail.com>
2020-02-27 08:51:42 +0400

    Update one-stop-seo.php

--- one-stop-seo.php
+++ one-stop-seo.php
@@ -3,7 +3,7 @@
  * Plugin Name: One Stop SEO
  * Plugin URI: 
  * Description: The is going to be the one and only one stop seo plugin Soon.
- * Version: 1.0
+ * Version: 1.1.2
  * Author: Fahad Bin Zafar
  * Author URI: https://www.linkedin.com/in/fahadbinzafar/
  */
